3a3f7e1d157ac023f5e4a093391555b42a21c806
xin <xin@snaar.net>
2017-11-26 19:29:21 -0500

    Use new file picker.

--- FileProtector/app/src/main/java/com/example/xin/fileprotector/MainActivity.java
+++ FileProtector/app/src/main/java/com/example/xin/fileprotector/MainActivity.java
@@ -1,19 +1,22 @@
 package com.example.xin.fileprotector;
 
+import android.Manifest;
 import android.app.Activity;
-import android.content.ContentResolver;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 import org.apache.commons.codec.binary.Hex;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.KeyStore;
@@ -22,8 +25,12 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.UnrecoverableEntryException;
 import java.security.cert.CertificateException;
+import java.util.ArrayList;
 
 import javax.crypto.NoSuchPaddingException;
+
+import droidninja.filepicker.FilePickerBuilder;
+import droidninja.filepicker.FilePickerConst;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,8 +40,9 @@ public class MainActivity extends AppCompatActivity {
     private DBHelper dbHelper;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         try {
@@ -50,53 +58,83 @@ public class MainActivity extends AppCompatActivity {
         fab.bringToFront();
     }
 
+    @Override
     protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        FileInfo fileInfo = new FileInfo();
-
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == 1) {
-                Uri uri = data.getData();
-                ContentResolver cr = getContentResolver();
-                java.io.File sourceFile = new java.io.File(uri.getPath());
-                java.io.File destFile = new java.io.File(getFilesDir(), sourceFile.getName() + "cipher");
-
-                try {
-                    boolean success = encryptor.encryptFile(cr.openInputStream(uri), destFile, ALIAS);
-                    if (success)
-                        Toast.makeText(this, "FILE ENCRYPTED", Toast.LENGTH_SHORT).show();
-                    else
-                        Toast.makeText(this, "FAIL: " +
-                                sourceFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                    Toast.makeText(this, "FAIL: FILE NOT FOUND" +
-                            sourceFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
-                }
-                catch (NoSuchPaddingException | NoSuchAlgorithmException | NoSuchProviderException
-                        | InvalidAlgorithmParameterException | InvalidKeyException
-                        | KeyStoreException | UnrecoverableEntryException e) {
-                    e.printStackTrace();
-                    Toast.makeText(this, "FAIL: EXCEPTION WHEN ENCRYPTING" +
-                            sourceFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
-                }
-
-                fileInfo.setEncryptedFileName(destFile.getAbsolutePath());
-                fileInfo.setOriginalPath(sourceFile.getAbsolutePath());
-                fileInfo.setType(FileInfo.FileType.Photo);
-                fileInfo.setKey(new String(Hex.encodeHex(encryptor.getIv())));
-
-                dbHelper.fileTable.addFile(fileInfo);
+        final ArrayList<String> files = new ArrayList<>();
+        switch (requestCode) {
+        case FilePickerConst.REQUEST_CODE_PHOTO:
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                files.addAll(data.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_MEDIA));
             }
+            break;
+        case FilePickerConst.REQUEST_CODE_DOC:
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                files.addAll(data.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_DOCS));
+            }
+            break;
+        }
+
+        encryptFiles(files);
+    }
+
+    private void encryptFiles(final ArrayList<String> files) {
+        for (final String fileName : files) {
+            final File file = new File(fileName);
+            encryptFile(file);
         }
     }
 
+    private void encryptFile(final File sourceFile) {
+        final InputStream is;
+        try {
+            is = new FileInputStream(sourceFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "FAIL: FILE NOT FOUND: " +
+                    sourceFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        final File destFile = new java.io.File(getFilesDir(), sourceFile.getName() + ".plp");
+
+        try {
+            final boolean success = encryptor.encryptFile(is, destFile, ALIAS);
+            if (success) {
+                Toast.makeText(this, "FILE ENCRYPTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "FAIL: " +
+                        sourceFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+            }
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException | NoSuchProviderException
+                | InvalidAlgorithmParameterException | InvalidKeyException
+                | KeyStoreException | UnrecoverableEntryException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "FAIL: EXCEPTION WHEN ENCRYPTING" +
+                    sourceFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+        }
+
+        final FileInfo fileInfo = new FileInfo();
+        fileInfo.setEncryptedFileName(destFile.getAbsolutePath());
+        fileInfo.setOriginalPath(sourceFile.getAbsolutePath());
+        fileInfo.setType(FileInfo.FileType.Photo);
+        fileInfo.setKey(new String(Hex.encodeHex(encryptor.getIv())));
+
+        dbHelper.fileTable.addFile(fileInfo);
+    }
+
     public void onClickViewAllFiles(final View view) {
-        final Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-        intent.setType("*/*");//FILE TYPE, */*stands for any type
-        startActivityForResult(intent,1);
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+        }
+
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "INSUFFICIENT PERMISSIONS", Toast.LENGTH_SHORT).show();
+        } else {
+            FilePickerBuilder.getInstance()
+                    .enableImagePicker(true)
+                    .enableVideoPicker(true)
+                    .pickPhoto(this);
+        }
     }
 
     public void onClickFolderPhoto(final View view) {
